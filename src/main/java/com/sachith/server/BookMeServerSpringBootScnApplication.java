a09d9060adfbeb2f3e46a75b9fe6d0cc9938d10d
Sachith-SL <itsmesachith@gmail.com>
2024-07-28 12:41:54 +0530

    Initial Commit

---
+++ src/main/java/com/sachith/server/BookMeServerSpringBootScnApplication.java
@@ -0,0 +1,13 @@
+package com.sachith.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMeServerSpringBootScnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMeServerSpringBootScnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachith/server/BookMeServerSpringBootScnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachith.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMeServerSpringBootScnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
